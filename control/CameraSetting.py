2ba90285cbee9e98d02112d4f7edc5042e2ffaff
YumaNagamine <y-nagamine829@asagi.waseda.jp>
2025-03-05 20:02:44 +0900

    create classes

---
+++ control/CameraSetting.py
@@ -0,0 +1,8 @@
+# This file is for shortening camera setting process created by Nagamine on March 5th
+
+class CameraSetting():
+    def __init__(self):
+        pass
+        
+    def main(self): #ここにカメラセッティングをすべて入れる
+        pass
---
+++ control/control_test.py
@@ -0,0 +1,12 @@
+# This file is for testing control process created by Nagemine on March 5th
+# NN + fuzzy feedback
+import os, sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+import numpy as np
+from cv_angle_traking.angles_reader_joint_estimation import AngleTracker
+import membership_function as mf
+from FuzzyFeedback import FuzzyFeedback
+from network import NeuralNetwork
+from control.CameraSetting import CameraSetting
