50bcc681b161086c5366332e9b1c68ad4a9b4f1f
YumaNagamine <y-nagamine829@asagi.waseda.jp>
2025-03-05 20:02:44 +0900

    create classes

---
+++ control/FuzzyFeedback.py
@@ -0,0 +1,13 @@
+# This class is for feedback control created by Nagemine on March 5th 
+
+import os, sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+import numpy as np
+from cv_angle_traking.angles_reader_joint_estimation import AngleTracker
+import membership_function as mf
+
+class FuzzyFeedback():
+    def __init__(self):
+        pass
